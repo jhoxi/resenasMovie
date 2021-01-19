9c71adea7694c78dc2020e3cf9ddbdda6ea4d5b8
jhoxi <jhoxaniuade@gmail.com>
2021-01-19 16:59:09 -0300

    Terminado

--- app.js
+++ app.js
@@ -14,38 +14,65 @@ app.listen(3030, () => console.log('Server running in 3030 port'));
 const heroes = JSON.parse(fs.readFileSync(__dirname + '/data/heroes.json', 'utf-8'));
 
 // Ruta Raíz / ➝ Home
-app.get('/', __¿?__);
+app.get('/', function(req, res){
+	let mensaje = 'Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en ti!.'
+	res.send(mensaje)
+});
+
 
 // Ruta /heroes ➝ se envía todo el array y Express lo parsea para el browser como JSON :D
-app.get('/heroes', (¿?) => {
-	res.send(ACÁ_DEBERÍAMOS_ENVIAR_A_TODOS_LOS_HÉROES);
+app.get('/heroes', (req, res) => {
+	res.send(heroes);
 });
+
+
 
 // Ruta /heroes/n ➝ se envía el nombre y profesión del héroe solicitado
-app.get('¿?', (¿?) => {
+app.get('/heroes/detalle/id', (req, res) => {
 	// Acá lo primero será encontrar al héroe que corresponda
-	let heroe = __¿?__;
-	
+	let heroe = heroes.find((heroe) => {
+	return heroe.id == req.params.id;
+		
 	// Si se encuentra al héroe se envía el nombre y su profesión
 	// Si NO se encuentra se envía el mensaje de no encontrado
-});
 
+	if (heroe) {
+		res.send(`${heroe.nombre}: ${heroe.profesion}`);
+	  } else {
+		res.send("No se encontro el heroe");
+	  }
+	});
+});
 // Ruta /heroes/n/bio ➝ se envía la bio del héroe solicitado
-app.get('¿?', (¿?) => {
+app.get("/heroes/bio/:id/:ok", (req, res) => {
 	// Acá lo primero será encontrar al héroe que corresponda
-	let heroe = __¿?__;
-
+	let heroe = heroes.find((heroe) => {
+	  return heroe.id == req.params.id;
+	});
+  
 	// Si NO se encuentra al héroe se envía un mensaje
-	// Si se encuentra al héroe:
-		// Se pregunta si vino el parámetro Y el valor esperado y se envía la información
-		// Si nó vino el parámetro se envía el mensaje de error
+	if (!heroe) {
+	  return res.send("No se encontro el heroe");
+	} else {
 	}
-});
-
-// Ruta Créditos
-// ¿?
-
-// Ruta... ¿Pára qué sirve esto?
-app.get('*', (req, res) => {
-	res.status(404).send('404 not found. <br> ¡Houston, poseemos problemas!');
-});
+	// Si se encuentra al héroe:
+  
+	// Si nó vino el parámetro se envía el mensaje de error
+	if (req.params.ok != "ok") {
+	  return res.send(
+		`${heroe.nombre}: Lamento que no desees saber más de mi :(`
+	  );
+	}
+  
+	// Si vino el parametro esperado, se envía la información
+	return res.send(`${heroe.nombre}: ${heroe.resenia}`);
+  });
+  
+  app.get("/creditos", (req, res) => {
+	return res.send(`Hecho por: Piarrot`);
+  });
+  
+  // Ruta... ¿Pára qué sirve esto?
+  app.get("*", (req, res) => {
+	res.status(404).send("404 not found. <br> ¡Houston, poseemos problemas!");
+  });
